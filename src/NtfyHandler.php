604400c1136afd1581c37de5e2876162db2763d6
cengiz <conkal@ciu.edu.tr>
2024-01-02 07:51:09 +0200

    php 8.1

--- src/NtfyHandler.php
+++ src/NtfyHandler.php
@@ -3,8 +3,10 @@
 namespace Conkal\NtfyMonologHandler;
 
 
+use Monolog\Level;
 use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\LogRecord;
 
 class NtfyHandler extends AbstractProcessingHandler
 {
@@ -12,21 +14,27 @@ class NtfyHandler extends AbstractProcessingHandler
 
     private $httpClient;
     private $endPoint;
+    private $username;
+    private $password;
 
-    public function __construct($endPoint, $httpClient, $level = Logger::DEBUG, $bubble = true)
+    public function __construct($endPoint, $httpClient, $level = Level::Debug, $bubble = true, $username = null, $password = null)
     {
         $this->httpClient = $httpClient;
         $this->endPoint = $endPoint;
+        $this->username = $username;
+        $this->password = $password;
         parent::__construct($level, $bubble);
     }
 
-    protected function write(array $record)
+    protected function write(LogRecord $record): void
     {
+        $auth = base64_encode($this->username.':'.$this->password);
         $this->httpClient->request('POST', $this->endPoint, [
             'headers' => [
                 'Content-Type' => 'text/plain',
                 'Title' => $record['message'],
-                'Priority' => $this->priority($record['level']),
+                'Priority' => $this->priority($record->level),
+                'Authorization'=>'Basic '.$auth,
             ],
             'body' => $record['message']
         ]);
@@ -34,20 +42,12 @@ class NtfyHandler extends AbstractProcessingHandler
 
     private function priority($level): int
     {
-        switch ($level) {
-            case Logger::NOTICE:
-            case Logger::WARNING:
-            case Logger::INFO:
-                return 2;
-            case Logger::ERROR:
-                return 3;
-            case Logger::CRITICAL:
-                return 4;
-            case Logger::EMERGENCY:
-            case Logger::ALERT:
-                return 5;
-            default:
-                return 1;
-        }
+        return match ($level) {
+            Level::Notice, Level::Debug, Level::Info => 2,
+            Level::Error => 3,
+            Level::Critical => 4,
+            Level::Emergency, Level::Alert => 5,
+            default => 1,
+        };
     }
 }
